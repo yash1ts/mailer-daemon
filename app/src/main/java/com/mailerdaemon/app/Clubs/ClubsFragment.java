193fb8397d8a4989b56181841c79362f2cc7cfc8
Yashwant <yashwantsandey2000@gmail.com>
2019-07-23 14:30:54 +0530

    upgraded fragment_club:          gridView -> recyclerGridView

--- app/src/main/java/com/mailerdaemon/app/Clubs/ClubsFragment.java
+++ app/src/main/java/com/mailerdaemon/app/Clubs/ClubsFragment.java
@@ -1,13 +1,11 @@
 package com.mailerdaemon.app.Clubs;
 
-import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
