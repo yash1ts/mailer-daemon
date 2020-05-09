be54291eaa3dea2d5148901e7469eca34dbecfc6
Yashwant Sandey <45488341+yash1ts@users.noreply.github.com>
2020-05-09 23:10:27 +0530

    Delete AppDatabase.java

--- app/src/main/java/com/mailerdaemon/app/attendance/Database/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.mailerdaemon.app.attendance.database;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Subject.class},version = 1,exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract SubjectDao subjectDao();
-}
