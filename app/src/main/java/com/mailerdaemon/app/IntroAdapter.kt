1068f3e4cdbfd8d2bab042f00a0d2e2536bc5654
Pratham2305 <Pratham2305@users.noreply.github.com>
2020-06-23 11:46:53 +0530

    Converted IntroActivity and IntroAdapter to kotlin.

---
+++ app/src/main/java/com/mailerdaemon/app/IntroAdapter.kt
@@ -0,0 +1,26 @@
+package com.mailerdaemon.app;
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.annotation.NonNull
+import androidx.viewpager.widget.PagerAdapter
+class IntroAdapter:PagerAdapter() {
+    private val xml = intArrayOf(R.layout.intro_1, R.layout.intro_2, R.layout.intro_3, R.layout.intro_4, R.layout.intro_5, R.layout.intro_6)
+
+    override fun isViewFromObject(@NonNull view:View, @NonNull `object`:Any):Boolean {
+        return (view === `object`)
+    }
+    @NonNull
+    override fun instantiateItem(@NonNull container:ViewGroup, position:Int):Any {
+        val view = LayoutInflater.from(container.context).inflate(xml[position], container, false)
+        container.addView(view)
+        return view
+    }
+    override fun destroyItem(@NonNull container:ViewGroup, position:Int, @NonNull `object`:Any) {
+        container.removeView(`object` as LinearLayout)
+    }
+
+    override fun getCount(): Int =xml.size
+}
