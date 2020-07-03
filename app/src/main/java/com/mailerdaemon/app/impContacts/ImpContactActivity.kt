79f3333a700f8aa1874c0b72965f3175c51e2dbf
Pratham2305 <Pratham2305@users.noreply.github.com>
2020-07-04 01:37:43 +0530

    Code Updated.

--- app/src/main/java/com/mailerdaemon/app/impContacts/ImpContactActivity.kt
+++ app/src/main/java/com/mailerdaemon/app/impContacts/ImpContactActivity.kt
@@ -4,85 +4,77 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mailerdaemon.app.R
 import com.mailerdaemon.app.WARDEN_LINK
+import com.mailerdaemon.app.toast
 import com.mailerdaemon.app.utils.ChromeTab
 import com.mailerdaemon.app.utils.ContactFunction
 import kotlinx.android.synthetic.main.activity_imp_contacts.*
 
 class ImpContactActivity : AppCompatActivity(), ContactFunction {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imp_contacts)
         setSupportActionBar(toolbar)
-        (supportActionBar)?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Contacts"
-        bt_faculty.setOnClickListener {
-        val tabs = arrayOf("All", "ACH", "AGL", "AGP", "MnC",
-        "AP", "CHE", "CIV", "CSE", "ECE",
-        "EE", "ESE", "FME", "HSS", "ME",
-        "MECH", "MME", "MS", "PE")
-        val pages = arrayOf("faculty_all", "ACH", "AGL", "AGP", "AM",
-        "AP", "CHE", "CIV", "CSE", "ECE",
-        "EE", "ESE", "FME", "HSS", "ME",
-        "MECH", "MME", "MS", "PE")
-        openDetail(tabs, pages) }
-        bt_admin.setOnClickListener {
-        val tabs = arrayOf("Deans", "Associate Deans", "HOD", "HOC")
-        val pages = arrayOf("deans", "associate_deans", "hod", "hoc")
-        openDetail(tabs, pages) }
-        bt_hostel.setOnClickListener {
-        val tab = ChromeTab(this)
-        tab.openTab(WARDEN_LINK) }
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.title = "Contacts"
         }
+        bt_faculty.setOnClickListener { openDetail(resources.getStringArray(R.array.tabs), resources.getStringArray(R.array.pages)) }
+        bt_admin.setOnClickListener {
+            val tabs = arrayOf("Deans", "Associate Deans", "HOD", "HOC")
+            val pages = arrayOf("deans", "associate_deans", "hod", "hoc")
+            openDetail(tabs, pages)
+        }
+        bt_hostel.setOnClickListener {
+            val tab = ChromeTab(this)
+            tab.openTab(WARDEN_LINK)
+        }
+    }
 
-private fun openDetail(tabs: Array<String>, pages: Array<String>) {
+    private fun openDetail(tabs: Array<String>, pages: Array<String>) {
         val fragment = ContactFragmentViewPager()
         val bundle = Bundle()
         bundle.putStringArray("tabs", tabs)
         bundle.putStringArray("pages", pages)
         fragment.arguments = bundle
         supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment, null).commit()
-        }
+    }
 
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId === android.R.id.home) onBackPressed()
         return true
-        }
+    }
 
-        override fun onBackPressed() {
+    override fun onBackPressed() {
         if (supportFragmentManager.fragments.isEmpty()) super.onBackPressed()
         else supportFragmentManager.beginTransaction().remove(supportFragmentManager.fragments[0]).commit()
-        }
+    }
 
-        fun openWebView(url: String) {
+    fun openWebView(url: String) {
         val fragment = HtmlFragment()
         val bundle = Bundle()
         bundle.putString("url", url)
         fragment.arguments = bundle
         supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment, null)
-        .addToBackStack(null).commit()
-        }
+                .addToBackStack(null).commit()
+    }
 
-        override fun makeCall(num: String) {
-        if (num.trim { it <= ' ' } != "0") {
-        val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + num.trim { it <= ' ' }))
-        startActivity(intent)
-        } else Toast.makeText(this, "Sorry number not available", Toast.LENGTH_LONG).show()
-        }
+    override fun makeCall(num: String) {
+        if (num.trim() != "0") {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + num.trim()))
+            startActivity(intent)
+        } else this.toast( "Sorry number not available")
+    }
 
-        override fun sendMail(s: String) {
-        if (s.trim { it <= ' ' }.isNotEmpty()) {
-        val send = Intent(Intent.ACTION_SENDTO)
-        val uriText = ("mailto:" + Uri.encode(s.trim { it <= ' ' }) +
-        "?subject=" + Uri.encode("Subject") +
-        "&body=" + Uri.encode("the body of the message"))
-        val uri = Uri.parse(uriText)
-        send.data = uri
-        startActivity(Intent.createChooser(send, "Send mail..."))
-        } else Toast.makeText(this, "Sorry email not available", Toast.LENGTH_LONG).show()
-        }
+    override fun sendMail(s: String) {
+        if (s.trim().isNotEmpty()) {
+            val send = Intent(Intent.ACTION_SENDTO)
+            val uriText = "mailto:${Uri.encode(s.trim())}?subject=${Uri.encode("Subject")}&body=${Uri.encode("the body of the message")}"
+            send.data = Uri.parse(uriText)
+            startActivity(Intent.createChooser(send, "Send mail..."))
+        } else this.toast("Sorry email not available")
+    }
 }
