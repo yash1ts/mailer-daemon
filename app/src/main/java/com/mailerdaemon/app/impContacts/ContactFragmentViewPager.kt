c422a05749c244174bfb5566aa8f65513062e47c
Pratham Pahariya <54663429+Pratham2305@users.noreply.github.com>
2020-07-14 00:24:07 +0530

    Converted ContactDetailFragment and ContactFragmentViewPager to kotlin. (#19)
    * Converted ContactDetailFragment and ContactViewPager
    
    Co-authored-by: Pratham2305 <Pratham2305@users.noreply.github.com>

---
+++ app/src/main/java/com/mailerdaemon/app/impContacts/ContactFragmentViewPager.kt
@@ -0,0 +1,42 @@
+package com.mailerdaemon.app.impContacts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.mailerdaemon.app.R
+import kotlinx.android.synthetic.main.fragment_contact_viewpager.view.*
+
+class ContactFragmentViewPager : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_contact_viewpager, container, false)
+        view.tab_layout.let {
+            it.setupWithViewPager(view.contact_viewpager)
+            it.tabMode = TabLayout.MODE_SCROLLABLE
+        }
+        val tabs = arguments?.getStringArray("tabs")
+        view.contact_viewpager.adapter = object : FragmentStatePagerAdapter(childFragmentManager,
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            var s = arguments?.getStringArray("pages")
+
+            override fun getItem(position: Int) = getFragment(s?.get(position))
+
+            override fun getCount() = s?.size ?: 0
+
+            override fun getPageTitle(position: Int) = tabs?.get(position)
+        }
+        return view
+    }
+    private fun getFragment(s: String?): Fragment {
+        val fragment = ContactDetailFragment()
+        val bundle = Bundle()
+        bundle.putString("type", s)
+        fragment.arguments = bundle
+        return fragment
+    }
+}
