6dabceba72730f1aaec8dd28f2b79d47642241a4
Pratham2305 <Pratham2305@users.noreply.github.com>
2020-07-27 17:11:13 +0530

    Code Updated.

--- app/src/main/java/com/mailerdaemon/app/impContacts/DiffUtilCallBack.kt
+++ app/src/main/java/com/mailerdaemon/app/impContacts/DiffUtilCallBack.kt
@@ -2,13 +2,15 @@ package com.mailerdaemon.app.impContacts
 
 import androidx.recyclerview.widget.DiffUtil
 
-class DiffUtilCallBack(private val mOldEmployeeList: List<Contact>, private val mNewEmployeeList: List<Contact>) : DiffUtil.Callback() {
+class DiffUtilCallBack(private val mOldEmployeeList: List<Contact>,
+                       private val mNewEmployeeList: List<Contact>) : DiffUtil.Callback() {
 
     override fun getOldListSize() = mOldEmployeeList.size
 
     override fun getNewListSize() = mNewEmployeeList.size
 
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) = (mOldEmployeeList[oldItemPosition].name == mNewEmployeeList[newItemPosition].name)
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int)
+        = (mOldEmployeeList[oldItemPosition].name == mNewEmployeeList[newItemPosition].name)
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         val (_, _, _, name) = mOldEmployeeList[oldItemPosition]
@@ -16,5 +18,6 @@ class DiffUtilCallBack(private val mOldEmployeeList: List<Contact>, private val 
         return name == name1
     }
 
-    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int) = super.getChangePayload(oldItemPosition, newItemPosition)
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int)
+        = super.getChangePayload(oldItemPosition, newItemPosition)
 }
