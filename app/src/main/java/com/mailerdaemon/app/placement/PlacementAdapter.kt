1fdfb5da99e6bd4f696cb15fe04e24b6d5d9e0b6
DhruvWalia03 <59091612+DhruvWalia03@users.noreply.github.com>
2020-12-22 11:46:44 +0530

    Update PlacementAdapter.kt

--- app/src/main/java/com/mailerdaemon/app/placement/PlacementAdapter.kt
+++ app/src/main/java/com/mailerdaemon/app/placement/PlacementAdapter.kt
@@ -13,7 +13,7 @@ import org.ocpsoft.prettytime.PrettyTime
 class PlacementAdapter(var list: List<PlacementModel>) : RecyclerView.Adapter<PlacementAdapter.Holder>() {
     val p = PrettyTime()
     val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US)
-    lateinit var title : String
+    lateinit var title: String
     lateinit var post : String
 
     class Holder(itemView: View) : RecyclerView.ViewHolder(itemView)
