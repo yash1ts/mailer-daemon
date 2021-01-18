6b9b824510ad91ee514e5140b50c654270f3082c
Pratham2305 <Pratham2305@users.noreply.github.com>
2021-01-19 00:05:17 +0530

    Gradle Flavors Added.

---
+++ app/src/admin/java/com/mailerdaemon/app/FlavorConstants.kt
@@ -0,0 +1,11 @@
+package com.mailerdaemon.app
+
+class FlavorConstants {
+    enum class Type {
+        STUDENT, ADMIN
+    }
+    companion object {
+        @JvmField
+        val type = Type.ADMIN
+    }
+}
