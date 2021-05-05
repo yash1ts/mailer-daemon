27da8869e4067bf2733f2676519584796a483394
DhruvWalia03 <waliadhruv3@gmail.com>
2021-05-05 11:19:49 +0530

    new commit

--- app/src/main/java/com/mailerdaemon/app/home/HomeFragment.java
+++ app/src/main/java/com/mailerdaemon/app/home/HomeFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -30,6 +31,7 @@ import com.mailerdaemon.app.utils.ContactFunction;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,6 +49,10 @@ public class HomeFragment extends Fragment implements ContactFunction {
     MaterialCardView bt_map;
     @BindView(R.id.exp_card)
     MaterialCardView bt_experience;
+    @BindView(R.id.textView4)
+    TextView placement;
+    @BindView(R.id.parent_portal)
+    LinearLayout bt_parent_portal;
     @BindView(R.id.calendar)
     LinearLayout bt_calander;
     @BindView(R.id.mess_menu)
@@ -66,10 +72,8 @@ public class HomeFragment extends Fragment implements ContactFunction {
         RvContacts.setAdapter(adapter);
         contactList= new Gson().fromJson(loadJSONFromAsset("contact"), ContactModel.class).getContact();
         if(contactList!=null)
-        adapter.setData(contactList);
-        bt_see_all.setOnClickListener(v->{
-            startActivity(new Intent(getActivity(),ImpContactActivity.class));
-        });
+            adapter.setData(contactList);
+        bt_see_all.setOnClickListener(v-> startActivity(new Intent(getActivity(),ImpContactActivity.class)));
         final long[] mLastClickTime = {0};
         bt_map.setOnClickListener(v->{
             if (SystemClock.elapsedRealtime() - mLastClickTime[0] < 1000) {
@@ -79,6 +83,7 @@ public class HomeFragment extends Fragment implements ContactFunction {
             startActivity(new Intent(getActivity(), MapsActivity.class));
         });
 
+        placement.setOnClickListener(v -> OpenPlacement());
         bt_experience.setOnClickListener(v->{
 //            Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:Tp9iD7YAcKo"));
 //            Intent webIntent = new Intent(Intent.ACTION_VIEW,
@@ -88,7 +93,11 @@ public class HomeFragment extends Fragment implements ContactFunction {
 //            } catch (ActivityNotFoundException ex) {
 //                Objects.requireNonNull(getContext()).startActivity(webIntent);
 //            }
-            startActivity(new Intent(getActivity(), PlacementActivity.class));
+            OpenPlacement();
+        });
+        bt_parent_portal.setOnClickListener(v->{
+            ChromeTab tab=new ChromeTab(getContext());
+            tab.openTab(ConstantsKt.Parent_Portal_Link);
         });
         bt_calander.setOnClickListener(v->{
             ChromeTab tab=new ChromeTab(getContext());
@@ -141,11 +150,13 @@ public class HomeFragment extends Fragment implements ContactFunction {
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            json = new String(buffer, "UTF-8");
+            json = new String(buffer, StandardCharsets.UTF_8);
         } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
         return json;
     }
+    public void OpenPlacement() {
+        startActivity(new Intent(getActivity(), PlacementActivity.class));}
 }
