56394b8d48d9f5dd4f62a61a292b4c12b6efda53
Pratham2305 <Pratham2305@users.noreply.github.com>
2021-07-05 00:13:23 +0530

    added viewbinding and other improvements

--- app/src/main/java/com/mailerdaemon/app/PostDetailActivity.kt
+++ app/src/main/java/com/mailerdaemon/app/PostDetailActivity.kt
@@ -5,58 +5,60 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.mailerdaemon.app.databinding.ActivityPostDetailBinding
 import com.mailerdaemon.app.notices.NoticeViewPagerAdapter
 import com.mailerdaemon.app.notices.PostModel
 import com.mailerdaemon.app.placement.PlacementModel
-import kotlinx.android.synthetic.main.activity_post_detail.*
-import kotlinx.android.synthetic.main.rv_notices.view.*
 
 class PostDetailActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityPostDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_detail)
-        bt_back.setOnClickListener { onBackPressed() }
-        val bundle = intent.extras
-        if (intent.getStringExtra("type").equals("Placement")) {
-            val postModel: PlacementModel = bundle?.getParcelable("post")!!
-            post_message.text = postModel?.message
-            post_tag.text = postModel?.message_tags[0].name
-            view_post_fb_layout.visibility = View.GONE
-            share_post.visibility = View.GONE
-            post_image_layout.visibility = View.GONE
-        } else {
-            val postModel: PostModel = bundle?.getParcelable("post")!!
-            post_message.text = postModel?.message
-            post_tag.text = postModel?.message_tags[0]
-
-            if (!postModel.permalink_url.isNullOrEmpty()) {
-                fb_post_button.setOnClickListener {
-                    val facebookIntent = Intent(Intent.ACTION_VIEW)
-                    facebookIntent.data = Uri.parse(postModel.permalink_url)
-                    startActivity(facebookIntent)
-                }
-                share_post.setOnClickListener {
-                    val shareIntent = Intent(Intent.ACTION_SEND)
-                    shareIntent.type = "text/plain"
-                    shareIntent.putExtra(Intent.EXTRA_TEXT, postModel.permalink_url)
-                    startActivity(Intent.createChooser(shareIntent, "Share..."))
-                }
-            }
-
-            if (postModel.full_picture.isNullOrBlank() || postModel.photo.isNotEmpty()) {
-                post_full_image.visibility = View.GONE
+        binding = ActivityPostDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.let {
+            it.btBack.setOnClickListener { onBackPressed() }
+            val bundle = intent.extras
+            if (intent.getStringExtra("type").equals("Placement")) {
+                val postModel: PlacementModel = bundle?.getParcelable("post")!!
+                it.postMessage.text = postModel?.message
+                it.postTag.text = postModel?.message_tags[0].name
+                it.viewPostFbLayout.visibility = View.GONE
+                it.sharePost.visibility = View.GONE
+                it.postImageLayout.visibility = View.GONE
             } else {
-                post_full_image.visibility = View.VISIBLE
-                post_full_image.setImageURI(postModel.full_picture)
-            }
-            if (postModel.photo.isNotEmpty()) {
-                val adapter = NoticeViewPagerAdapter()
-                post_viewpager.visibility = View.VISIBLE
-                adapter.list = postModel.photo
-                post_viewpager.adapter = adapter
-            } else {
-                post_viewpager.visibility = View.GONE
+                val postModel: PostModel = bundle?.getParcelable("post")!!
+                it.postMessage.text = postModel?.message
+                it.postTag.text = postModel?.message_tags[0]
+
+                if (!postModel.permalink_url.isNullOrEmpty()) {
+                    it.fbPostButton.setOnClickListener {
+                        val facebookIntent = Intent(Intent.ACTION_VIEW)
+                        facebookIntent.data = Uri.parse(postModel.permalink_url)
+                        startActivity(facebookIntent)
+                    }
+                    it.sharePost.setOnClickListener {
+                        val shareIntent = Intent(Intent.ACTION_SEND)
+                        shareIntent.type = "text/plain"
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, postModel.permalink_url)
+                        startActivity(Intent.createChooser(shareIntent, "Share..."))
+                    }
+                }
+
+                if (postModel.full_picture.isNullOrBlank() || postModel.photo.isNotEmpty()) {
+                    it.postFullImage.visibility = View.GONE
+                } else {
+                    it.postFullImage.visibility = View.VISIBLE
+                    it.postFullImage.setImageURI(postModel.full_picture)
+                }
+                if (postModel.photo.isNotEmpty()) {
+                    val adapter = NoticeViewPagerAdapter()
+                    it.postViewpager.visibility = View.VISIBLE
+                    adapter.list = postModel.photo
+                    it.postViewpager.adapter = adapter
+                } else {
+                    it.postViewpager.visibility = View.GONE
+                }
             }
         }
     }
